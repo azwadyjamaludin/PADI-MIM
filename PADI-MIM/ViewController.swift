0ae7bb5ffb28bf10b4a694687d832e843968b5c4
Azi Azwady Jamaludin <aziazwady@gmail.com>
2019-01-17 12:12:31 +0800

    Initial Commit

---
+++ PADI-MIM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PADI-MIM
+//
+//  Created by Azi Azwady Jamaludin on 17/01/2019.
+//  Copyright © 2019 Azi Azwady Jamaludin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
